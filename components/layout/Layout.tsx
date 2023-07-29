5aedb0f7f476e91daf9e211710f7966f0ab5e6c3
Lachlan Sleight <lachlansleight@gmail.com>
2023-07-29 15:23:51 +1000

    fix: Add version number to header

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Head from "next/head";
+import packageJson from "../../package.json";
 
 const Layout = ({ children }: { children: ReactNode }): JSX.Element => {
     return (
@@ -10,7 +11,10 @@ const Layout = ({ children }: { children: ReactNode }): JSX.Element => {
             </Head>
             <main className="min-h-screen bg-neutral-800 text-white w-screen">
                 <div className="h-8 bg-neutral-900 flex justify-center items-center text-xl italic shadow-md">
-                    Now Soon Later
+                    Now Soon Later{" "}
+                    <span className="text-xs relative top-1 left-2 text-white text-opacity-50">
+                        v{packageJson.version}
+                    </span>
                 </div>
                 <div className="px-2 mt-2">{children}</div>
             </main>
